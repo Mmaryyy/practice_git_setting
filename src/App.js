bc06f4919cb05105227294f5467735ce7816adeb
Mmaryyy <marynrvly@gmail.com>
2023-02-14 16:25:33 +0900

    dev에서 pr 날리기

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          dev에서 pr 날리기
         </a>
       </header>
     </div>
