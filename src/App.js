58f63354c4e491598fbf208d62ba014072bf7795
Mmaryyy <marynrvly@gmail.com>
2023-02-14 16:38:33 +0900

    dev에서 pr 날리기3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          dev에서 pr 날리기2
+          dev에서 pr 날리기3
         </a>
       </header>
     </div>
